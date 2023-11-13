5c0f7f3078a9695ee571d2131237210583ca4908
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-11-13 14:51:59 +0000

    JSX, Components and Props

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <User name="Elena" age={33} email="elena@yahoo.com"/>
+      <br></br>
+      <User name="Rodica" age={32} email="rodicas@yahoo.com"/>
+      <br></br>
+      <User name="Mona" age={45} email="mona@yahoo.com"/>
     </div>
   );
 }
+
+const User = (props) => {
+  return (
+    <div>
+      <h1><i>{props.name}</i></h1>
+      <h2>{props.age}</h2>
+      <h3><em>{props.email}</em></h3>
+    </div> 
+  );
+};
+
 
 export default App;
