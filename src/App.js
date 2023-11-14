f6f9e131280bdef577165839b581761388578463
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-11-14 11:04:07 +0000

    components and props exercises

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <User name="Elena" age={33} email="elena@yahoo.com"/>
+      <About name= "Rodica" surname="Negura" age={32} ocupation= "WebDeveloper"/>
       <br></br>
-      <User name="Rodica" age={32} email="rodicas@yahoo.com"/>
+      <About name= "Bogdan" surname="Negura" age={33} ocupation= "WebDeveloper"/>
       <br></br>
-      <User name="Mona" age={45} email="mona@yahoo.com"/>
+      <About name= "Iasmina" surname="Negura" age={7} ocupation= "bebeWebDeveloper"/>
+      <br></br>
+      <About name= "Damian" surname="Negura" age={5} ocupation= "bebeWebDeveloper"/>
     </div>
   );
 }
 
-const User = (props) => {
+const About = ({name, surname, age, ocupation}) => {
   return (
     <div>
-      <h1><i>{props.name}</i></h1>
-      <h2>{props.age}</h2>
-      <h3><em>{props.email}</em></h3>
-    </div> 
-  );
-};
-
+      <h1>{name}</h1>
+      <h2>{surname}</h2>
+      <h1>{age}</h1>
+      <h3>{ocupation}</h3>
+    </div>
+  )
+}
 
 export default App;
