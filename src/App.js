2221023a9c044ceb67735f19d3e521bbdbb2f6c6
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-11-15 13:04:37 +0000

    working with Lists- exerice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import './App.css';
-import {Car} from '../src/Car';
+// import {Car} from '../src/Car';
 
 function App() {
-  const cars = [
-    {name: "Volkswagen", cp: 190 }, 
-    {name: "Volvo", cp: 178}, 
-    {name: "Audi", cp: 189}, 
-    {name: "Skoda", cp: 160}, 
-    {name: "BMW", cp:190},
+  const planets = [
+    {name: "Mars", isGasPLanet: false }, 
+    {name: "Earth", isGasPLanet: false}, 
+    {name: "Jupiter", isGasPLanet: true}, 
+    {name: "Venus", isGasPLanet: false}, 
+    {name: "Neptune", isGasPLanet: true},
+    {name: "Uranus", isGasPLanet: true},
   ];
   
   return (
     <div className="App">
-      {cars.map((car, key) => {
-        return (
-          < Car name={car.name} cp={car.cp}/>
-        )
-      })}
+      {planets.map((planet, key) => !planet.isGasPLanet && <h1>{planet.name}</h1>)}
+      
     </div>
   );
 }
