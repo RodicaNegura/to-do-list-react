984a521ed00c6d73b483dba67696fdcad035fed3
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-11-15 11:09:33 +0000

    added a boolean "isGreen"

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import './App.css';
 
 function App() {
   const age = 20;
+  const isGreen = true;
   return (
     <div className="App">
       { age >= 18 ? <h1>Over Age</h1> : <h1>Under Age</h1>}
+      <h1 style={{ color: isGreen ? "green" :  "red"}}> This Has Color</h1>
     </div>
   );
 }
