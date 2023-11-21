82e666f6ccfc18c784ec7dfd8648564af7c4a452
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-11-21 11:28:30 +0000

    creating links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Home from "./pages/home"
 import Menu from './pages/menu';
@@ -10,6 +10,11 @@ const  App = () => {
   return  ( 
     <div className='App'>
       <Router>
+        <div>
+          <Link to="/"> HOME </Link>
+          <Link to="/menu"> MENU </Link>
+          <Link to="/contacts"> CONTACTS </Link>
+        </div>
         <Routes>
           <Route path='/' element={<Home />}/>
           <Route path='/menu' element={<Menu />}/>
