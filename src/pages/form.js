2d1c2e0cb5b555935a5fde48088b859c0d46cf77
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-11-24 11:41:32 +0000

    React Hook Form

--- src/pages/form.js
+++ src/pages/form.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const Form = () => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data)
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="Full Name..."/>
-      <input type="text" placeholder="Email..."/>
-      <input type="text" placeholder="Age..."/>
-      <input type="password" placeholder="Password..."/>
-      <input type="password" placeholder="Confirm Password..."/>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" placeholder="Full Name..." {...register("fullName")} />
+      <br></br>
+      <br></br>
+      <input type="text" placeholder="Email..." {...register("email")}/>
+      <br></br>
+      <input type="number" placeholder="Age..." {...register("age")}/>
+      <br></br>
+      <input type="password" placeholder="Password..." {...register("password")}/>
+      <br></br>
+      <input type="password" placeholder="Confirm Password..." {...register("confirmPassword")}/>
+      <br></br>
       <input type="submit" />
     </form>
   )
