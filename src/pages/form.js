8a3bb6e7ca6bd16c2944da664434cd95221af315
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-11-24 11:17:27 +0000

    install react-hook-form and yup

--- src/pages/form.js
+++ src/pages/form.js
@@ -2,15 +2,14 @@ import React from "react";
 
 const Form = () => {
   return (
-      <form>
-        <input type="text" placeholder="Full Name..."/>
-        <input type="text" placeholder="Email..."/>
-        <input type="text" placeholder="Age..."/>
-        <input type="password" placeholder="Password..."/>
-        <input type="password" placeholder="Confirm Password..."/>
-        <input type="submit" />
-      </form>
-   
+    <form>
+      <input type="text" placeholder="Full Name..."/>
+      <input type="text" placeholder="Email..."/>
+      <input type="text" placeholder="Age..."/>
+      <input type="password" placeholder="Password..."/>
+      <input type="password" placeholder="Confirm Password..."/>
+      <input type="submit" />
+    </form>
   )
 }
 
