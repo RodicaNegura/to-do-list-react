a43e9e65519058deea5c42973034bc235475b442
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-11-24 11:13:12 +0000

    create a new page for project

--- src/App.js
+++ src/App.js
@@ -1,37 +1,14 @@
 import './App.css';
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Home from "./pages/home"
-import Profile from './pages/profile';
-import Contact from './pages/contact';
-import Navbar from './navbar';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import Form from './pages/form';
 
 
 
 const  App = () => {
-  const client = new QueryClient({
-    defaultOptions: {
-      queries: {
-        refetchOnWindowFocus: false, // used to not updated data when you move the pages
-      },
-    },
-    
-  });
+  
   return  ( 
     <div className='App'>
-      <QueryClientProvider client={client}>
-        <Router>
-          <Navbar/>
-          <Routes>
-            <Route path='/' element={<Home />}/>
-            {/* <Route path="/profile" element={<Profile />}/> */}
-            <Route path="/contact" element={<Contact />}/>
-            <Route path='*' element={<h1> Page Not Found </h1>}/>
-          </Routes>
-        </Router>
-      </QueryClientProvider>
+      <Form />
     </div>
   );
 }
