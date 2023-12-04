509d0828a1286de6305787e1d343cb952bafe31a
RodicaNegura <RodicaElenaNegura@gmail.com>
2023-12-04 12:24:55 +0000

    added new button show/hide

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import { useToggle } from "../src/pages/useToggle"
 // import Form from './pages/form';
 
 const  App = () => {
-  const [isVisible, toggle] = useToggle()
+  const [isVisible, toggle] = useToggle();
+  const [isVisible2, toggle2] = useToggle();
   
 
   return  (  
     <div className='App'>
       <button onClick={toggle}>{isVisible ? "Hide" : "Show"}</button>
       {isVisible && <h1> Hidden Text</h1>}
+      {/* <Form /> */}
+      <button onClick={toggle2}>{isVisible2 ? "Hide" : "Show"}</button>
+      {isVisible2 && <h1> Hidden Text</h1>}
       {/* <Form /> */}
     </div>
   );
